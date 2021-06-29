89289e8deea7a3f9c97e31e465e5011dd68e9096
Anak7n <buzzzzx233@gmail.com>
2021-06-29 13:11:17 +0800

    feat: 添加 http 代替 fetch 做请求，useHttp 请求时添加上用户的 token

---
+++ src/utils/http.js
@@ -0,0 +1,53 @@
+import * as qs from "qs";
+import * as auth from "../auth-provider";
+import { useAuth } from "../context/auth-context";
+import { useCallback } from "react";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+export const http = (
+  endpoint,
+  { data, token, headers, ...customConfig } = {}
+) => {
+  const config = {
+    method: "GET",
+    headers: {
+      Authorization: token ? `Bearer ${token}` : "",
+      "Content-Type": data ? "application/json" : "",
+    },
+    ...customConfig,
+  };
+
+  if (config.method.toUpperCase() === "GET") {
+    endpoint += `?${qs.stringify(data)}`;
+  } else {
+    config.body = JSON.stringify(data || {});
+  }
+
+  // axios 和 fetch 的行为不同：axios 可以在返回状态不为 2xx 时返回异常，fetch 不会返回异常
+  return window
+    .fetch(`${apiUrl}/${endpoint}`, config)
+    .then(async (response) => {
+      if (response.status === 401) {
+        await auth.logout();
+        window.location.reload();
+        return Promise.reject({ message: "请重新登录" });
+      }
+
+      const data = await response.json();
+      if (response.ok) {
+        return data;
+      } else {
+        return Promise.reject(data);
+      }
+    });
+};
+
+export const useHttp = () => {
+  const { user } = useAuth();
+
+  return useCallback(
+    (endpoint, config) => http(endpoint, { ...config, token: user?.token }),
+    [user?.token]
+  );
+};
